71921ea4c95769a4068264354f29435eecf81755
Mola <129961195+mljadama@users.noreply.github.com>
2025-11-06 11:45:02 +0000

    Update supabaseClient.js

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-
-// REPLACE WITH YOUR SUPABASE PROJECT DETAILS
+//Connection to supabase
 const supabaseUrl = 'https://fhvriauekkfpzoieruap.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZodnJpYXVla2tmcHpvaWVydWFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIzMjg4NjUsImV4cCI6MjA3NzkwNDg2NX0.syGBd43DMhTUkFIkFwqZqmX2pt1XnArnm6sECBXOYWc';
 
